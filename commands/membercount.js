15c45746d83da869c00f13a76ddeee263c10402f
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-12 20:18:30 +0100

    Update membercount.js

--- commands/membercount.js
+++ commands/membercount.js
@@ -1,6 +1,10 @@
 module.exports = {
     aliases: ['members'],
     run: (message, args) => {
-        message.channel.send(`${guild.name} has ${guild.memberCount} members`);
+        client.guilds.cache.forEach((guild) => {
+            message.channel.send(`${guild.name} has ${guild.memberCount} members`);
+        });
+
+
     }
 }
