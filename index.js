20090ceee66827aef0eb73b1fd0823881e499f6b
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-12 18:48:45 +0100

    simplified code to avoid errors

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
-const Discord = require('discord.js');
-const WOKCommands = require('wokcommands');
-const client  = new Discord.Client();
+const DiscordJS = require('discord.js')
+const WOKCommands = require('wokcommands')
 
-const prefix = '.';
-const fs = require('fs');
-client.commands = new Discord.Collection();
+const client = new DiscordJS.Client()
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
-
-    client.commands.set(command.name, command);
-}
-
-
-client.once('ready', () => {
-    new WOKCommands(client, 'commands', 'features')
-    console.log('ready');
+client.on('ready', () => {
+  // Initialize WOKCommands
+  new WOKCommands(client, 'commands', 'features')
 })
-
-
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if(command === "ping"){
-        client.commands.get("ping").execute(message, args);
-    }
-    else if (command == "pog") {
-        client.commands.get("pog").execute(message, args);
-    }
-    else if (command == "invite") {
-        client.commands.get("invite").execute(message, args);
-    }
-});
-``
 
 client.login(process.env.BOT_TOKEN)
