690a33a6ebcef984afd433a74d043a953c5dfd6a
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-12 20:56:35 +0100

    Progress

--- commands/pog.js
+++ commands/pog.js
@@ -1,6 +1,6 @@
 module.exports = {
     aliases: [],
-    run: (message, args) => {
+    run: (message, args, text, client, prefix, instance) => {
         message.channel.send('https://cdn.discordapp.com/emojis/718439375792242738.png?v=1');
     }
 }
