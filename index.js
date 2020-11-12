4d25e9eb553a1435057756fea8747e1541c9bb92
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-12 19:01:38 +0100

    changed default prefix to "."

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const client = new DiscordJS.Client()
 client.on('ready', () => {
   // Initialize WOKCommands
   new WOKCommands(client, 'commands', 'features')
+  .setDefaultPrefix('.')
 })
 
 client.login(process.env.BOT_TOKEN)
