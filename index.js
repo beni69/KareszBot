043796beb2ed79a08f9b8d24c391248828698704
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-12 19:06:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const client = new DiscordJS.Client()
 
 client.on('ready', () => {
   // Initialize WOKCommands
-  new WOKCommands(client, 'commands', 'features')
-  .setDefaultPrefix('.')
+  new WOKCommands(client, 'commands', 'features').setDefaultPrefix('.')
 })
 
 client.login(process.env.BOT_TOKEN)
