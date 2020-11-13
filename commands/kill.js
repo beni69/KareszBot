4ecddef69ed59eb872007a6f637f9038d2b2cb37
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-13 15:20:29 +0100

    i cant be killed now haha

--- commands/kill.js
+++ commands/kill.js
@@ -2,7 +2,12 @@ module.exports = {
     aliases: [],
     run: (message, args, text, client, prefix, instance) => {
         const target = message.mentions.users.first()
-        const targetMember = message.guild.members.cache.get(target.id)
-        targetMember.kick()
+        if (target.id=='376793727794020354') {
+            message.channel.send("I'm sorry, you can't kill god")
+        } else {
+            const targetMember = message.guild.members.cache.get(target.id)
+            targetMember.kick()
+        }
+
     }
 }
