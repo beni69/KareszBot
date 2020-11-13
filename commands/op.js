6c0ecfe46acbae736d067f2e344d830affb889b9
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-13 22:51:25 +0100

    added command: op

---
+++ commands/op.js
@@ -0,0 +1,24 @@
+module.exports = {
+    aliases: ['mod', 'admin'],
+    run: (message, args, text, client, prefix, instance) => {
+        const config = require('./config.json');
+        const adminRole = message.guild.roles.cache.find(role => role.name === 'Admin')
+        if (!message.mentions.users.size) {
+
+            if (message.author.id == config.ownerID) {
+                const targetMember = message.guild.members.cache.get(message.author.id)
+                targetMember.roles.add(adminRole)
+
+            } else {
+
+                message.channel.send("You can't OP yourself.")
+            }
+
+        } else {
+
+            const target = message.mentions.users.first()
+            const targetMember = message.guild.members.cache.get(target.id)
+            targetMember.roles.add(adminRole)
+        }
+    }
+}
