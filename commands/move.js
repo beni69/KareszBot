2a5bb47509818171a55e4c5b0da248d4e779bad5
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-13 18:10:30 +0100

    updated move.js

--- commands/move.js
+++ commands/move.js
@@ -1,5 +1,7 @@
 module.exports = {
     aliases: ['brazil'],
+    minArgs: 1,
+    maxArgs: -1,
     run: (message, args, text, client, prefix, instance) => {
         const member = message.mentions.members.first()
         member.voice.setChannel('771089486263091250')
