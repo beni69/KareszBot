df982cc313a44ac42fb3d13b34f6f33a4949f220
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-13 20:37:15 +0100

    prefix should now be .

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const DiscordJS = require('discord.js')
 const WOKCommands = require('wokcommands')
+const config = require('./config.json');
 
 const client = new DiscordJS.Client()
-const prefix = '.'
 
 client.on('ready', () => {
+    console.log('Bot ready');
   // Initialize WOKCommands
-  new WOKCommands(client, 'commands', 'features').setDefaultPrefix(prefix)
+  new WOKCommands(client, 'commands', 'features').setDefaultPrefix(config.prefix)
 })
 
-client.login(process.env.BOT_TOKEN)
+client.login('Nzc2NTI2NTc0NDgwNTIzMjc1.X62Kug.0g7kZDBLBTZ5218Bg1aBkm4KZQE')
