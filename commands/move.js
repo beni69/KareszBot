821a519a70812f8f159a0bc2502c290e747ccfff
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-13 22:19:31 +0100

    updated move.js

--- commands/move.js
+++ commands/move.js
@@ -1,9 +1,18 @@
 module.exports = {
     aliases: ['brazil'],
-    minArgs: 1,
+    minArgs: 0,
     maxArgs: -1,
     run: (message, args, text, client, prefix, instance) => {
-        const member = message.mentions.members.first()
-        member.voice.setChannel('771089486263091250')
+        const config = require('../config.json');
+        if (!message.mentions.users.size) {
+
+            const target = message.guild.members.cache.get(message.author.id)
+            target.voice.setChannel(config.brazil)
+
+        } else {
+
+            const member = message.mentions.members.first()
+            member.voice.setChannel(config.brazil)
+        }
     }
 }
