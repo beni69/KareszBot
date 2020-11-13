6b896ab70c16ada13637bfa6311146985f8d1390
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-13 16:51:03 +0100

    added command: poll
    please note that the target server needs to have upvote and downvote emojis

---
+++ commands/poll.js
@@ -0,0 +1,11 @@
+module.exports = {
+    aliases: ['vote'],
+    run: (message, args, text, client, prefix, instance) => {
+        const upvote = message.guild.emojis.cache.find(emoji => emoji.name === 'upvote');
+        message.react(upvote);
+        setTimeout(() => {
+            const downvote = message.guild.emojis.cache.find(emoji => emoji.name === 'downvote');
+            message.react(downvote);
+        })
+    }
+}
