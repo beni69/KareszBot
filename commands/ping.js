994fc5a3ff99435c6f341db2da816769c5663ecf
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-13 16:33:35 +0100

    ping is no longer negative

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 module.exports = {
     aliases: ['p'],
     run: (message, args, text, client, prefix, instance) => {
-        message.channel.send(`🏓 ${message.createdTimestamp-Date.now()}`);
+        message.channel.send(`🏓 ${Date.now()-message.createdTimestamp}`);
     }
 }
