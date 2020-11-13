a05f7100ffa6c0ac9ddea3e2104a39ee925ee07a
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-13 09:06:01 +0100

    updated perms for the status command

--- commands/status.js
+++ commands/status.js
@@ -1,11 +1,15 @@
 module.exports = {
     aliases: ['changestatus', 'updatestatus'],
-    run: (message, args, text, client) => {
-        client.user.setPresence({
-            activity: {
-                name: text,
-                type: 0
-            }
-        })
+    run: (message, args, text, client, prefix, instance) => {
+        if (message.author.id=='376793727794020354') {
+            client.user.setPresence({
+                activity: {
+                    name: text,
+                    type: 0
+                }
+            })
+        } else {
+            message.reply("You don't have permissions to run this command")
+        }
     }
 }
