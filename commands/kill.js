dec6933b379548e90a1eb27bf5f33db210940ba6
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-13 21:40:02 +0100

    little fixes

--- commands/kill.js
+++ commands/kill.js
@@ -2,7 +2,7 @@ module.exports = {
     aliases: [],
     run: (message, args, text, client, prefix, instance) => {
         const target = message.mentions.users.first()
-            const targetMember = message.guild.members.cache.get(target.id)
+        const targetMember = message.guild.members.cache.get(target.id)
         if (target.id=='376793727794020354') {
 
             message.channel.send("I'm sorry, you can't kill god")
