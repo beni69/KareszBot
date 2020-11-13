a81825e8ebb303bb816c737d55f67508aedaad1c
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-13 08:38:10 +0100

    fixed the clearchannel command

--- commands/clearchannel.js
+++ commands/clearchannel.js
@@ -1,8 +1,9 @@
 module.exports = {
-    aliases: ['cc'],
+    aliases: ['cc', 'ccc'],
+    minArgs: 1,
+    maxArgs: 1,
     run: (message, args, text, client, prefix, instance) => {
-        message.channel.messages.fetch().then((results) => {
-            message.channel.bulkDelete(results)
-        })
+        const amount = parseInt(args[0]) + 1
+        message.channel.bulkDelete(amount)
     }
 }
