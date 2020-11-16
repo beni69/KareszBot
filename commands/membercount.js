a04b00eb2a842292ca9623639b72c7a1934b3348
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-16 17:00:31 +0100

    removed membercont in favor of serverinfo

--- commands/membercount.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    aliases: ['members'],
-    run: (message, args, text, client, prefix, instance) => {
-        message.channel.send(`${message.guild.name} has ${message.guild.memberCount} members`);
-    }
-}
