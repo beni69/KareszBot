9ec759b351010ee73bbd780d194ebe9d93c1530a
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-17 11:28:45 +0100

    Update pog.js

--- commands/pog.js
+++ commands/pog.js
@@ -1,6 +1,8 @@
 module.exports = {
     aliases: [],
     run: (message, args, text, client, prefix, instance) => {
+        // const pog = message.guild.emojis.cache.find(emoji => emoji.name === 'pog');
+        // message.react(pog);
         message.channel.send('https://cdn.discordapp.com/emojis/718439375792242738.png?v=1');
     }
 }
