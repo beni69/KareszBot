33b7cc75167e23ddeeedea98463b18db884bf62b
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-17 11:59:52 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,10 @@
 module.exports = {
     aliases: ['p'],
     run: (message, args, text, client, prefix, instance) => {
+        const config = require('../config.json');
         message.channel.send(`🏓  ${Date.now()-message.createdTimestamp}ms`);
+
+        const channel = message.guild.channels.cache.get(config.logChannel)
+        channel.send(`<@${message.member.id}> ran: ${message.content}`)
     }
 }
