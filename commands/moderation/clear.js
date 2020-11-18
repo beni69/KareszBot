8160e610e16ad2bc4267123105cd541aa51bd419
beni69 <73666440+beni69@users.noreply.github.com>
2020-11-18 22:12:45 +0100

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -5,6 +5,7 @@ module.exports = {
     run: (message, args, text, client, prefix, instance) => {
         const config = require('../../config.json');
         const cmdlog = require('../../features/commandLog.js');
+        
         const amount = parseInt(args[0]) + 1
         message.channel.bulkDelete(amount)
 
