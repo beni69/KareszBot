16c1ca09daf9cbdfd4dceea6bc98065ea0124ecb
beni69 <73666440+beni69@users.noreply.github.com>
2020-11-18 19:51:42 +0100

    Update invite.js

--- commands/information/invite.js
+++ commands/information/invite.js
@@ -3,7 +3,7 @@ module.exports = {
     run: (message, args, text, client, prefix, instance) => {
         const config = require('../../config.json');
         const cmdlog = require('../../features/commandLog.js');
-        message.channel.send('Use this link to invite the bot to other servers:\nhttps://bit.ly/3ngmM5G');
+        message.channel.send('Invite me to other servers:\nhttps://bit.ly/3ngmM5G');
         // message.channel.send('https://bit.ly/3ngmM5G');
 
         cmdlog.Log(client, message);
