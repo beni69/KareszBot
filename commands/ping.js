391fee907c845f9f0bbe86b7008da4333b213fd0
TheGameWizard12 <73666440+TheGameWizard12@users.noreply.github.com>
2020-11-18 07:55:39 +0100

    removed the 'p' alias to avoid confusion with 'play'

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,5 @@
 module.exports = {
-    aliases: ['p'],
+    aliases: [],
     run: (message, args, text, client, prefix, instance) => {
         const config = require('../config.json');
         message.channel.send(`🏓  ${Date.now()-message.createdTimestamp}ms`);
