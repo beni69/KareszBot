97b9c89a703cedad98146e31f03757b7a9144f9c
beni69 <73666440+beni69@users.noreply.github.com>
2020-11-19 16:19:05 +0100

    pushing brazil.js to test out the all arg

--- commands/fun/brazil.js
+++ commands/fun/brazil.js
@@ -5,7 +5,14 @@ module.exports = {
     run: (message, args, text, client, prefix, instance) => {
         const config = require('../../config.json');
         const cmdlog = require('../../features/commandLog.js');
-        if (!message.mentions.users.size) {
+        if (args[0] == 'all') {
+
+            message.guild.members.cache.forEach(member => {
+                //guard clause, early return
+                if(member.id === message.member.id || !member.voice.channel) return;
+                member.voice.setChannel(config.voice.brazil);
+            });
+        } else if (!message.mentions.users.size) {
 
             const target = message.guild.members.cache.get(message.author.id)
             target.voice.setChannel(config.voice.brazil)
