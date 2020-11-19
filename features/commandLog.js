9bc5f30a2ea3f9ca85eb6d8cadadcb08c50eeede
beni69 <73666440+beni69@users.noreply.github.com>
2020-11-19 13:59:30 +0100

    Update commandLog.js

--- features/commandLog.js
+++ features/commandLog.js
@@ -2,6 +2,5 @@
 
 exports.Log = function (client, message) {
     const config = require('../config');
-    // trying to make command logging easier
     client.guilds.cache.get(config.testServer).channels.cache.get(config.logChannel).send(`<@${message.member.id}> in **${message.guild.name}**:    ${message.content}`)
 };
