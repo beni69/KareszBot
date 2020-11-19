7929c7df87e793af6e7e99640d3589a70192b5ce
beni69 <73666440+beni69@users.noreply.github.com>
2020-11-19 14:00:36 +0100

    kill is now a lot more powerful

--- commands/moderation/kill.js
+++ commands/moderation/kill.js
@@ -5,29 +5,35 @@ module.exports = {
     run: (message, args, text, client, prefix, instance) => {
         const config = require('../../config.json');
         const cmdlog = require('../../features/commandLog.js');
-        const target = message.mentions.users.first()
-        const targetMember = message.guild.members.cache.get(target.id)
+
+        const target = message.mentions.users.first();
+        const targetMember = message.guild.members.cache.get(target.id);
+
         if (target.id == config.owner.id) {
 
-            message.channel.send("I'm sorry, you can't kill god")
+            message.channel.send("I'm sorry, you can't kill god");
 
-        } else {
-            if (targetMember.roles.cache.some(role => role.name === 'Admin')) {
+        } else if (target.id == message.guild.ownerID) {
 
-                const adminRole = message.guild.roles.cache.find(role => role.name === 'Admin')
-                targetMember.roles.remove(adminRole)
-                // console.log('admin role removed')
+            message.channel.send("Sadly, you can't kick the server owner")
 
+        } else if (target.bot) {
+
+            for (var i = 0; i < targetMember._roles.length; i++) {
+                const role = message.guild.roles.cache.find(role => role.id == targetMember._roles[i]);
+                if (role.name != target.username) {
+                    // console.log('name good');
+                    targetMember.roles.remove(role);
+                }
             }
-            if (targetMember.roles.cache.some(role => role.name === 'Bot')) {
 
-                const botRole = message.guild.roles.cache.find(role => role.name === 'Bot')
-                targetMember.roles.remove(botRole)
-                // console.log('bot role removed')
-            }
-            console.log(targetMember);
             targetMember.kick()
-            message.channel.send(`${targetMember.user.username} fell off a cliff`)
+        } else {
+
+            for (var i = 0; i < targetMember._roles.length; i++) {
+                const role = message.guild.roles.cache.find(role => role.id == targetMember._roles[i]);
+                targetMember.roles.remove(role).catch(console.error)
+            }
         }
 
         cmdlog.Log(client, message);
