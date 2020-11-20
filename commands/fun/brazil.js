5b2d85c0ee4026c706ab1f4d0a644b8a516d6c94
beni69 <73666440+beni69@users.noreply.github.com>
2020-11-20 11:06:38 +0100

    added @everyone as an alias to all
    bot should now move the message author too

--- commands/fun/brazil.js
+++ commands/fun/brazil.js
@@ -5,25 +5,25 @@ module.exports = {
     run: (message, args, text, client, prefix, instance) => {
         const config = require('../../config.json');
         const cmdlog = require('../../features/commandLog.js');
-        if (args[0] == 'all') {
+        if (args[0] == 'all' || args[0] == '@everyone') {
 
             message.guild.members.cache.forEach(member => {
                 //guard clause, early return
-                if(member.id === message.member.id || !member.voice.channel) return;
+                if (!member.voice.channel) return;
                 member.voice.setChannel(config.voice.brazil);
             });
         } else if (!message.mentions.users.size) {
 
-            const target = message.guild.members.cache.get(message.author.id)
-            target.voice.setChannel(config.voice.brazil)
+            const target = message.guild.members.cache.get(message.author.id);
+            target.voice.setChannel(config.voice.brazil);
 
         } else {
 
-            const member = message.mentions.members.first()
-            member.voice.setChannel(config.voice.brazil)
+            const member = message.mentions.members.first();
+            member.voice.setChannel(config.voice.brazil);
         }
-        message.react('🇧🇷')
+        message.react('🇧🇷');
 
         cmdlog.Log(client, message);
     }
-}
+};
