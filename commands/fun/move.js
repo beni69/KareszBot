1fa860e9978c38e63d6769282c541b7e923b4195
beni69 <73666440+beni69@users.noreply.github.com>
2020-11-22 20:17:47 +0100

    update move.js

--- commands/fun/move.js
+++ commands/fun/move.js
@@ -6,21 +6,6 @@ module.exports = {
         const config = require('../../config.json');
         const cmdlog = require('../../features/commandLog.js');
 
-
-        if (args.length == 1) {
-            vc = client.channels.cache.find(channel => channel.name == args[0]);
-            const member = message.guild.members.cache.get(message.author.id);
-            member.voice.setChannel(vc);
-
-        } else {
-            vc = client.channels.cache.find(channel => channel.name == args[1]);
-            const member = message.mentions.members.first();
-            member.voice.setChannel(vc);
-        }
-
-
-
-
         if (!message.mentions.users.size) {
             vc = client.channels.cache.find(channel => channel.name == text);
             const member = message.guild.members.cache.get(message.author.id);
