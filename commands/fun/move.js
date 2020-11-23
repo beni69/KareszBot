2e39a4866d188b3610a4b68df98f2c29ad728707
beni69 <73666440+beni69@users.noreply.github.com>
2020-11-23 16:10:18 +0100

    move now has @everyone support

--- commands/fun/move.js
+++ commands/fun/move.js
@@ -6,22 +6,36 @@ module.exports = {
         const config = require('../../config.json');
         const cmdlog = require('../../features/commandLog.js');
 
-        if (!message.mentions.users.size) {
-            const vc = client.channels.cache.find(channel => channel.name == text);
-            message.member.voice.setChannel(vc);
-        } else {
+        if (args[0] == 'all' || args[0] == '@everyone') {
+
             let cName = text.replace(args[0], '');
             while (cName.charAt(0) == ' ') {
                 cName = cName.substring(1);
             }
+
+            const vc = client.channels.cache.find(channel => channel.name == cName);
+            message.guild.members.cache.forEach(member => {
+                //guard clause, early return
+                if (!member.voice.channel) return;
+                member.voice.setChannel(vc);
+            });
+
+        } else if (!message.mentions.users.size) {
+
+            const vc = client.channels.cache.find(channel => channel.name == text);
+            message.member.voice.setChannel(vc);
+
+        } else {
+
+            let cName = text.replace(args[0], '');
+            while (cName.charAt(0) == ' ') {
+                cName = cName.substring(1);
+            }
+
             const vc = client.channels.cache.find(channel => channel.name == cName);
             const member = message.mentions.members.first();
             member.voice.setChannel(vc);
         }
-
-
-
-
 
 
         message.react('👌');
