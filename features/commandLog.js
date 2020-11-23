e3d1167072b1ad3b46e1b470ca3120d5dce2c832
beni69 <73666440+beni69@users.noreply.github.com>
2020-11-23 16:09:22 +0100

    commandlog should now take in custom inputs

--- features/commandLog.js
+++ features/commandLog.js
@@ -1,6 +1,10 @@
 // trying to make command logging easier
 
-exports.Log = function(client, message) {
+exports.Log = function(client, message, c) {
     const config = require('../config');
-    client.guilds.cache.get(config.testServer).channels.cache.get(config.logChannel).send(`<@${message.member.id}> in **${message.guild.name}**:    ${message.content}`);
+    if (c) {
+        client.guilds.cache.get(config.testServer).channels.cache.get(config.logChannel).send(c);
+    } else {
+        client.guilds.cache.get(config.testServer).channels.cache.get(config.logChannel).send(`<@${message.member.id}> in **${message.guild.name}**:    ${message.content}`);
+    }
 };
