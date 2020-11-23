5f720b0bd76c09e685307377130427275877f7d4
beni69 <73666440+beni69@users.noreply.github.com>
2020-11-23 11:02:35 +0100

    fu mate

--- commands/fun/brazil.js
+++ commands/fun/brazil.js
@@ -5,22 +5,35 @@ module.exports = {
     run: (message, args, text, client, prefix, instance) => {
         const config = require('../../config.json');
         const cmdlog = require('../../features/commandLog.js');
+
+        if (message.author.id == config.mate) return;
+
+        const brazil = client.guilds.cache.get(config.nyf).channels.cache.get(config.voice.brazil);
+
         if (args[0] == 'all' || args[0] == '@everyone') {
 
             message.guild.members.cache.forEach(member => {
                 //guard clause, early return
                 if (!member.voice.channel) return;
-                member.voice.setChannel(config.voice.brazil);
+                member.voice.setChannel(brazil);
             });
         } else if (!message.mentions.users.size) {
 
             const target = message.guild.members.cache.get(message.author.id);
-            target.voice.setChannel(config.voice.brazil);
+            if (!target.voice.channel) {
+                message.channel.send('You need to be in a voice channel first.');
+                return;
+            }
+            target.voice.setChannel(brazil);
 
         } else {
 
             const member = message.mentions.members.first();
-            member.voice.setChannel(config.voice.brazil);
+            if (!member.voice.channel) {
+                message.channel.send('Your friend needs to be in a voice channel first.');
+                return;
+            }
+            member.voice.setChannel(brazil);
         }
         message.react('🇧🇷');
 
