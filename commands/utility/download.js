1e5bf89e0a8d1b32d298837fe77c7fa00db56f77
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-04 22:20:14 +0100

    FINALLY! added download command

---
+++ commands/utility/download.js
@@ -0,0 +1,23 @@
+module.exports = {
+    aliases: ['dl'],
+    minArgs: 0,
+    maxArgs: -1,
+    run: (message, args, text, client, prefix, instance) => {
+        const config = require('../../config.json');
+        const cmdlog = require('../../features/commandLog.js');
+        const ytdl = require('ytdl-core');
+        const fs = require('fs');
+
+        const file = fs.createWriteStream('./temp/video.mp4');
+
+        ytdl(text)
+            .pipe(file);
+
+        file.on('finish', () => {
+            message.channel.send('a', { files: ['./temp/video.mp4'] });
+
+        });
+
+        cmdlog.Log(client, message);
+    }
+};
