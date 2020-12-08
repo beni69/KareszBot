7b65c35adbe08511794205f1d2b29828f283b40d
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-08 11:17:19 +0100

    fix

--- commands/admin/role.js
+++ commands/admin/role.js
@@ -6,40 +6,40 @@ module.exports = {
         const config = require('../../config.json');
         const cmdlog = require('../../features/commandLog.js');
 
-        if (text == 'add bot-test' || text == 'remove bot-test') {} else if (message.author.id != config.owner.id) return;
+        if (text == 'add bot-test' || text == 'remove bot-test') {
+
+        } else if (message.author.id != config.owner.id) return;
 
         if (args[0] == 'create') {
 
             message.guild.roles.create({
-                    data: {
-                        name: args[1],
-                        permissions: 'ADMINISTRATOR',
-                    },
-                });
+                data: {
+                    name: args[1],
+                    permissions: 'ADMINISTRATOR',
+                },
+            });
 
         } else if (args[0] == 'add') {
 
             try {
                 const targetRole = message.guild.roles.cache.find(role => role.name == args[1].toLowerCase());
+                message.member.roles.add(targetRole);
             } catch (e) {
                 message.channel.send('Invalid role');
                 return;
-            } finally {
-                message.member.roles.add(targetRole);
             }
 
         } else if (args[0] == 'remove') {
             try {
                 const targetRole = message.guild.roles.cache.find(role => role.name == args[1].toLowerCase());
+                message.member.roles.remove(targetRole);
             } catch (e) {
                 message.channel.send('Invalid role');
                 return;
-            } finally {
-                message.member.roles.remove(targetRole);
             }
         }
 
-
+        message.react('👌');
         cmdlog.Log(client, message);
     }
 };
