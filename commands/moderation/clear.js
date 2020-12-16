ee38d6c5635bd8d23390af7a4d72ea5b4dbd8dba
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-16 18:59:02 +0100

    :heavy_plus_sign: new feature
    clear command now logs deleted messages

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -2,15 +2,25 @@ module.exports = {
     aliases: ['c', 'cc'],
     minArgs: 1,
     maxArgs: 1,
-    run: (message, args, text, client, prefix, instance) => {
+    run: async (message, args, text, client, prefix, instance) => {
         const config = require('../../config.json');
         const cmdlog = require('../../features/commandLog.js');
 
+        await message.channel.bulkDelete(1, true).catch(err => console.error(err));
         const amount = parseInt(args[0]) + 1;
-        message.channel.bulkDelete(amount, true).catch(err => {
-            console.error(err);
-        });
 
-        cmdlog.Log(client, message);
+        let deleted = [];
+        for (let i = 1; i < amount; i++) {
+            await message.channel.messages.fetch({ limit: 1 })
+                .then(message => {
+                    const item = Array.from(message)[0][1];
+                    deleted.push(item.content);
+                })
+                .catch(err => console.error(err));
+            await message.channel.bulkDelete(1, true).catch(err => console.error(err));
+        }
+        console.log(deleted);
+
+        cmdlog.Log(client, message, `<@${message.member.id}> in **${message.guild.name}**:    Deleted ${deleted.length} messages: **${deleted.reverse().join('**, **')}**`);
     }
 };
