20ba359c23274d9857a15f8ad29fd45901a05f1a
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-16 19:24:20 +0100

    fix

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -7,20 +7,15 @@ module.exports = {
         const cmdlog = require('../../features/commandLog.js');
 
         await message.channel.bulkDelete(1, true).catch(err => console.error(err));
-        const amount = parseInt(args[0]) + 1;
 
+        const amount = parseInt(args[0]);
+        let ray = await message.channel.messages.fetch({ limit: amount });
+        ray = Array.from(ray);
         let deleted = [];
-        for (let i = 1; i < amount; i++) {
-            await message.channel.messages.fetch({ limit: 1 })
-                .then(message => {
-                    const item = Array.from(message)[0][1];
-                    deleted.push(item.content);
-                })
-                .catch(err => console.error(err));
-            await message.channel.bulkDelete(1, true).catch(err => console.error(err));
-        }
-        console.log(deleted);
+        await ray.forEach(item => {
+            deleted.push(item[1].content);
+        });
 
-        cmdlog.Log(client, message, `<@${message.member.id}> in **${message.guild.name}**:    Deleted ${deleted.length} messages: **${deleted.reverse().join('**, **')}**`);
+        cmdlog.Log(client, message, `<@${message.member.id}> in **${message.guild.name}**:    Deleted ${amount} messages: **${deleted.reverse().join('**, **')}**`);
     }
 };
