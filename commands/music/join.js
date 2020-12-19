555689fd3664f87c65d21ec26fbb150f69a5dbe0
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-19 14:26:06 +0100

    Update join.js

--- commands/music/join.js
+++ commands/music/join.js
@@ -1,71 +1,40 @@
+let GetChan = function (client, message, chan) {
+	const config = require("../../config.json");
+	const cmdlog = require("../../features/commandLog.js");
 
+	let vc;
 
-// function Join(client, message, chan) {
-//     const config = require('../../config.json');
-//     const cmdlog = require('../../features/commandLog.js');
-//
-//     let vc;
-//
-//     if (!chan) {
-//         if (!message.member.voice.channelID) {
-//             message.channel.send('Join a voice channel first, dumbass');
-//             return;
-//         } else {
-//             vc = message.member.voice.channel;
-//             vc.join();
-//         }
-//     } else {
-//         try {
-//             vc = client.channels.cache.find(channel => channel.name == chan);
-//             vc.join();
-//         } catch (e) {
-//             message.reply('Invalid channel name');
-//             return;
-//         } finally {}
-//     }
-//
-//     cmdlog.Log(client, message, `<@${message.member.id}>: Joined **${vc.name}** in **${message.guild.name}**`);
-// }
-//
-// exports.functionName = function () {
-//     // body...
-// };
+	if (!chan) {
+		if (!message.member.voice.channelID) {
+			message.channel.send("Join a voice channel first, dumbass");
+			return "failed";
+		} else {
+			vc = message.member.voice.channel;
+			return vc;
+		}
+	} else {
+		try {
+			vc = client.channels.cache.find(channel => channel.name == chan);
+			return vc;
+		} catch (e) {
+			message.reply("Invalid channel name");
+			return "failed";
+		}
+	}
 
-
-let GetChan = function (client, message, chan){
-    const config = require('../../config.json');
-    const cmdlog = require('../../features/commandLog.js');
-
-    let vc;
-
-    if (!chan) {
-        if (!message.member.voice.channelID) {
-            message.channel.send('Join a voice channel first, dumbass');
-            return 'failed';
-        } else {
-            vc = message.member.voice.channel;
-            return vc;
-        }
-    } else {
-        try {
-            vc = client.channels.cache.find(channel => channel.name == chan);
-            return vc;
-        } catch (e) {
-            message.reply('Invalid channel name');
-            return 'failed';
-        }
-    }
-
-    cmdlog.Log(client, message, `<@${message.member.id}>: Joined **${vc.name}** in **${message.guild.name}**`);
+	cmdlog.Log(
+		client,
+		message,
+		`<@${message.member.id}>: Joined **${vc.name}** in **${message.guild.name}**`
+	);
 };
 module.exports = GetChan;
 
-
 module.exports = {
-    aliases: [],
-    minArgs: 0,
-    maxArgs: -1,
-    run: (message, args, text, client, prefix, instance) => {
-        GetChan(client, message, text).join();
-    }
+	aliases: ["j"],
+	minArgs: 0,
+	maxArgs: -1,
+	run: (message, args, text, client, prefix, instance) => {
+		GetChan(client, message, text).join();
+	},
 };
