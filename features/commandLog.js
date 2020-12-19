cdadd6b433c5ec1894093c2da37993bd04be5b8d
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-19 15:31:43 +0100

    :bug: fix

--- features/commandLog.js
+++ features/commandLog.js
@@ -1,18 +1,18 @@
 exports.Log = function (client, message, c, startup = false) {
 	const config = require("../config");
 	if (startup) {
-		client.guild.cache
+		client.guilds.cache
 			.get(config.testServer)
 			.channels.cache.get(config.logChannel)
 			.send(c);
-		console.log("Karesz: Bot ready!");
+		console.log("Karesz > Bot ready!");
 	} else if (c) {
 		client.guilds.cache
 			.get(config.testServer)
 			.channels.cache.get(config.logChannel)
 			.send(c);
 		console.log(
-			`Karesz: Running command: ${message.content}, requested by: ${message.author.tag}\nProvided log message: ${c}`
+			`Karesz > Running command: ${message.content}, requested by: ${message.author.tag}\nProvided log message: ${c}`
 		);
 	} else {
 		client.guilds.cache
@@ -22,7 +22,7 @@ exports.Log = function (client, message, c, startup = false) {
 				`<@${message.member.id}> in **${message.guild.name}**:    ${message.content}`
 			);
 		console.log(
-			`Karesz: Running command: ${message.content}, requested by: ${message.author.tag}`
+			`Karesz > Running command: ${message.content}, requested by: ${message.author.tag}`
 		);
 	}
 };
