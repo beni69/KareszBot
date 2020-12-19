8231a1c0d65013e3ef43fa3077f7a39286d38eae
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-19 14:55:31 +0100

    Update commandLog.js

--- features/commandLog.js
+++ features/commandLog.js
@@ -1,22 +1,28 @@
-exports.Log = function (client, message, c) {
-    const config = require("../config");
-    if (c) {
-        client.guilds.cache
-            .get(config.testServer)
-            .channels.cache.get(config.logChannel)
-            .send(c);
-        console.log(
-            `Karesz: Running command: ${message.content}, requested by: ${message.author.tag}\nProvided log message: ${c}`
-        );
-    } else {
-        client.guilds.cache
-            .get(config.testServer)
-            .channels.cache.get(config.logChannel)
-            .send(
-                `<@${message.member.id}> in **${message.guild.name}**:    ${message.content}`
-            );
-        console.log(
-            `Karesz: Running command: ${message.content}, requested by: ${message.author.tag}`
-        );
-    }
+exports.Log = function (client, message, c, startup = false) {
+	const config = require("../config");
+	if (startup) {
+		client.guild.cache
+			.get(config.testServer)
+			.channels.cache.get(config.logChannel)
+			.send(c);
+		console.log("Karesz: Bot ready!");
+	} else if (c) {
+		client.guilds.cache
+			.get(config.testServer)
+			.channels.cache.get(config.logChannel)
+			.send(c);
+		console.log(
+			`Karesz: Running command: ${message.content}, requested by: ${message.author.tag}\nProvided log message: ${c}`
+		);
+	} else {
+		client.guilds.cache
+			.get(config.testServer)
+			.channels.cache.get(config.logChannel)
+			.send(
+				`<@${message.member.id}> in **${message.guild.name}**:    ${message.content}`
+			);
+		console.log(
+			`Karesz: Running command: ${message.content}, requested by: ${message.author.tag}`
+		);
+	}
 };
