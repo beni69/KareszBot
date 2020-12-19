20b8b793a9aabfc80cb44e15251a6c0696bfbe35
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-19 20:45:48 +0100

    :bug: fix

--- features/reaction.js
+++ features/reaction.js
@@ -1,14 +1,16 @@
-exports.Simple = function (client, message, input, prop = 'name') {
-    try {
-        let emoji;
-        if (prop === 'name') {
-            emoji = message.guild.emojis.cache.find(emoji => emoji.name === input);
-        } else if (prop === 'id') {
-            emoji = client.emojis.cache.get(input);
-        }
+exports.Simple = function (client, message, input, prop = "name") {
+	try {
+		let emoji;
+		if (prop === "name") {
+			emoji = message.guild.emojis.cache.find(
+				emoji => emoji.name === input
+			);
+		} else if (prop === "id") {
+			emoji = client.emojis.cache.get(input);
+		}
 
-        message.react(emoji);
-    } catch (err) {
-        throw err;
-    }
+		message.react(emoji);
+	} catch (err) {
+		console.error(err);
+	}
 };
