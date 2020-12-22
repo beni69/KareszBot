451d31fd3795fe9b4131c8bf358591dc601b598d
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-22 13:15:59 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,11 @@
 module.exports = {
-    aliases: [],
-    run: (message, args, text, client, prefix, instance) => {
-        const config = require('../config.json');
-        const cmdlog = require('../features/commandLog.js');
-        message.channel.send(`🏓  ${client.ws.ping}ms`);
+	aliases: [],
+	run: (message, args, text, client, prefix, instance) => {
+		const config = require("../config.json");
+		const cmdlog = require("../features/commandLog.js");
 
-        cmdlog.Log(client, message);
-    }
+		message.channel.send(`🏓  ${client.ws.ping}ms`);
+
+		cmdlog.Log(client, message);
+	},
 };
