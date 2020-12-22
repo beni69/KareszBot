1635510cafde0c3ebebf397d22da55b40524af05
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-22 14:43:23 +0100

    Update avatar.js

--- commands/information/avatar.js
+++ commands/information/avatar.js
@@ -1,23 +1,27 @@
 module.exports = {
-    aliases: ['profilepic', 'pfp', 'getpic'],
-    minArgs: 0,
-    maxArgs: 1,
-    run: (message, args, text, client, prefix, instance) => {
-        const config = require('../../config.json');
-        const cmdlog = require('../../features/commandLog.js');
-        if (!message.mentions.users.size) {
-            message.channel.send(message.author.displayAvatarURL({
-                format: "png",
-                dynamic: true
-            }));
-        } else {
-            const user = message.mentions.users.first();
-            message.channel.send(user.displayAvatarURL({
-                format: "png",
-                dynamic: true
-            }));
-        }
+	aliases: ["profilepic", "pfp", "getpic"],
+	minArgs: 0,
+	maxArgs: 1,
+	run: (message, args, text, client, prefix, instance) => {
+		const config = require("../../config.json");
+		const cmdlog = require("../../features/commandLog.js");
+		if (!message.mentions.users.size) {
+			message.channel.send(
+				message.author.displayAvatarURL({
+					format: "png",
+					dynamic: true,
+				})
+			);
+		} else {
+			const user = message.mentions.users.first();
+			message.channel.send(
+				user.displayAvatarURL({
+					format: "png",
+					dynamic: true,
+				})
+			);
+		}
 
-        cmdlog.Log(client, message);
-    }
+		cmdlog.Log(client, message);
+	},
 };
