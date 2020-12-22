039dc8bc28a3b6a5edefd77e0799f2344704fae5
beni69 <73666440+beni69@users.noreply.github.com>
2020-12-22 14:43:17 +0100

    Update help.js

--- commands/information/help.js
+++ commands/information/help.js
@@ -1,37 +1,45 @@
 module.exports = {
-    aliases: ['karesz'],
-    run: (message, args, text, client, prefix, instance) => {
-        const config = require('../../config.json');
-        const cmdlog = require('../../features/commandLog.js');
-        const Discord = require('discord.js');
-        const embed = {
-            color: 'BLURPLE',
-            title: 'Karesz',
-            description: '**Command**\n*Usage*',
-            thumbnail: {url: config.karesz.pfp},
+	aliases: ["karesz"],
+	run: (message, args, text, client, prefix, instance) => {
+		const config = require("../../config.json");
+		const cmdlog = require("../../features/commandLog.js");
 
-            fields: [
-                // {name: 'Command name', value: 'usage\n\n'},
-                {name: 'ping', value: 'ping'},
-                {name: 'Clear', value: 'clear <amount>'},
-                {name: 'Poll', value: 'poll <message>'},
-                {name: 'Kill', value: 'kill <@someone>'},
-                {name: 'Orbi', vaule: 'orbi'},
-                {name: 'move', value: 'move [@someone] <channel>'},
-                {name: 'brazil', value: 'brazil [@someone] *or* brazil all'},
-                {name: 'snake', value: 'snake'},
-                {name: 'op', value: 'op <@someone>'},
-                {name: 'deop', value: 'deop <@someone>'},
-                {name: 'Profilepic', value: 'pfp [@someone]'},
-                {name: 'Serverinfo', value: 'info'}
-            ],
-            timestamp: new Date(),
-            footer: {
-                text: message.author.tag
-            }
-        };
-        message.channel.send({embed: embed});
+		const ownerPFP = client.users.cache
+			.find(user => user.id == config.owner.id)
+			.displayAvatarURL({format: "png", dynamic: true});
 
-        cmdlog.Log(client, message);
-    }
+		const embed = {
+			color: "BLURPLE",
+			title: "Karesz",
+			author: {
+				name: "Karesz by Beni#7999",
+				icon_url: ownerPFP,
+			},
+			description: "**Command**\n*Usage*",
+			thumbnail: {url: config.karesz.pfp},
+
+			fields: [
+				// {name: 'Command name', value: 'usage\n\n'},
+				{name: "ping", value: "ping"},
+				{name: "Clear", value: "clear <amount>"},
+				{name: "Poll", value: "poll <message>"},
+				{name: "Kill", value: "kill <@someone>"},
+				{name: "Orbi", vaule: "orbi"},
+				{name: "move", value: "move [@someone] <channel>"},
+				{name: "brazil", value: "brazil [@someone] *or* brazil all"},
+				{name: "snake", value: "snake"},
+				{name: "op", value: "op <@someone>"},
+				{name: "deop", value: "deop <@someone>"},
+				{name: "Profilepic", value: "pfp [@someone]"},
+				{name: "Serverinfo", value: "info"},
+			],
+			timestamp: new Date(),
+			footer: {
+				text: message.author.tag,
+			},
+		};
+		message.channel.send({embed: embed});
+
+		cmdlog.Log(client, message);
+	},
 };
