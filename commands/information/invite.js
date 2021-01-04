c0c054ced2ec4b63984da50f1c980f83f7e944e8
beni69 <73666440+beni69@users.noreply.github.com>
2021-01-04 14:18:09 +0100

    new invite link

--- commands/information/invite.js
+++ commands/information/invite.js
@@ -3,10 +3,8 @@ module.exports = {
     run: ({message, args, text, client, prefix, instance}) => {
         const config = require("../../config.json");
         const cmdlog = require("../../features/commandLog.js");
-        message.channel.send(
-            "Invite me to other servers:\nhttps://bit.ly/karesz"
-        );
-        // message.channel.send('https://bit.ly/3ngmM5G');
+
+        message.channel.send(`Invite me to other servers:\n${config.inv}`);
 
         cmdlog.Log(client, message);
     },
