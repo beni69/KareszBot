dd275bf139e58e6f2e951e523d7f3bdcf0ae30f5
beni69 <73666440+beni69@users.noreply.github.com>
2021-01-08 12:31:13 +0100

    support for longer strings

--- commands/fun/ascii.js
+++ commands/fun/ascii.js
@@ -5,22 +5,32 @@ module.exports = {
         const cmdlog = require("../../features/commandLog.js");
         const figlet = require("figlet");
 
-        figlet(text, (err, data) => {
-            if (err) {
-                message.reply("Error while creating ascii");
-                console.log("Karesz > Eror while creating ascii");
-                console.dir(err);
-                return;
+        figlet.text(
+            text,
+            {
+                horizontalLayout: "default",
+                verticalLayout: "fitted",
+                width: 140,
+                whitespaceBreak: true,
+            },
+            (err, data) => {
+                if (err) {
+                    message.reply("Error while creating ascii");
+                    console.log("Karesz > Eror while creating ascii");
+                    console.dir(err);
+                    return;
+                }
+                const ascii = `\`\`\`\n${data}\n\`\`\``;
+                // console.log(ascii.length);
+                if (ascii.length > 2000) {
+                    message.reply(
+                        "Sorry, that message would be over the discord character limit (2000 chars)"
+                    );
+                    console.log("Karesz > Eror while sending ascii");
+                } else message.channel.send(ascii);
             }
-            const ascii = `\`\`\`\n${data}\n\`\`\``;
-            if (ascii.length > 2000) {
-                message.reply(
-                    "Sorry, that message would be over the discord character limit (2000 chars)"
-                );
-                console.log("Karesz > Eror while sending ascii");
-            } else message.channel.send(ascii);
-        });
+        );
 
-        // cmdlog.Log(client, message);
+        cmdlog.Log(client, message);
     },
 };
