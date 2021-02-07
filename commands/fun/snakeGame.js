61dbf8cdaa0336951698554eb24f46957430308d
beni69 <73666440+beni69@users.noreply.github.com>
2021-02-07 13:55:47 +0100

    formatting

--- commands/fun/snakeGame.js
+++ commands/fun/snakeGame.js
@@ -1,13 +1,13 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 const WIDTH = 15;
 const HEIGHT = 10;
 const gameBoard = [];
-const apple = { x: 1, y: 1 };
+const apple = {x: 1, y: 1};
 
 class SnakeGame {
     constructor() {
-        this.snake = [{ x: 5, y: 5 }];
+        this.snake = [{x: 5, y: 5}];
         this.snakeLength = 1;
         this.score = 0;
         this.gameEmbed = null;
@@ -20,7 +20,7 @@ class SnakeGame {
     }
 
     gameBoardToString() {
-        let str = ""
+        let str = "";
         for (let y = 0; y < HEIGHT; y++) {
             for (let x = 0; x < WIDTH; x++) {
                 if (x == apple.x && y == apple.y) {
@@ -36,8 +36,7 @@ class SnakeGame {
                     }
                 }
 
-                if (flag)
-                    str += gameBoard[y * WIDTH + x];
+                if (flag) str += gameBoard[y * WIDTH + x];
             }
             str += "\n";
         }
@@ -46,39 +45,41 @@ class SnakeGame {
 
     isLocInSnake(pos) {
         return this.snake.find(sPos => sPos.x == pos.x && sPos.y == pos.y);
-    };
+    }
 
     newAppleLoc() {
-        let newApplePos = { x: 0, y: 0 };
+        let newApplePos = {x: 0, y: 0};
         do {
-            newApplePos = { x: parseInt(Math.random() * WIDTH), y: parseInt(Math.random() * HEIGHT) };
-        } while (this.isLocInSnake(newApplePos))
+            newApplePos = {
+                x: parseInt(Math.random() * WIDTH),
+                y: parseInt(Math.random() * HEIGHT),
+            };
+        } while (this.isLocInSnake(newApplePos));
 
         apple.x = newApplePos.x;
         apple.y = newApplePos.y;
     }
 
     newGame(msg) {
-        if (this.inGame)
-            return;
+        if (this.inGame) return;
 
         this.inGame = true;
         this.score = 0;
         this.snakeLength = 1;
-        this.snake = [{ x: 5, y: 5 }];
+        this.snake = [{x: 5, y: 5}];
         this.newAppleLoc();
         const embed = new Discord.MessageEmbed()
-            .setColor('#03ad03')
-            .setTitle('Snake Game')
+            .setColor("#03ad03")
+            .setTitle("Snake Game")
             .setDescription(this.gameBoardToString())
             .setTimestamp();
 
         msg.channel.send(embed).then(emsg => {
             this.gameEmbed = emsg;
-            this.gameEmbed.react('⬅️');
-            this.gameEmbed.react('⬆️');
-            this.gameEmbed.react('⬇️');
-            this.gameEmbed.react('➡️');
+            this.gameEmbed.react("⬅️");
+            this.gameEmbed.react("⬆️");
+            this.gameEmbed.react("⬇️");
+            this.gameEmbed.react("➡️");
 
             this.waitForReaction();
         });
@@ -92,8 +93,8 @@ class SnakeGame {
         }
 
         const editEmbed = new Discord.MessageEmbed()
-            .setColor('#03ad03')
-            .setTitle('Snake Game')
+            .setColor("#03ad03")
+            .setTitle("Snake Game")
             .setDescription(this.gameBoardToString())
             .setTimestamp();
         this.gameEmbed.edit(editEmbed);
@@ -104,63 +105,71 @@ class SnakeGame {
     gameOver() {
         this.inGame = false;
         const editEmbed = new Discord.MessageEmbed()
-            .setColor('#03ad03')
-            .setTitle('Snake Game')
+            .setColor("#03ad03")
+            .setTitle("Snake Game")
             .setDescription("GAME OVER!\nSCORE: " + this.score)
             .setTimestamp();
         this.gameEmbed.edit(editEmbed);
 
-        this.gameEmbed.reactions.removeAll()
+        this.gameEmbed.reactions.removeAll();
     }
 
     filter(reaction, user) {
-        return ['⬅️', '⬆️', '⬇️', '➡️'].includes(reaction.emoji.name) && user.id !== this.gameEmbed.author.id;
+        return (
+            ["⬅️", "⬆️", "⬇️", "➡️"].includes(reaction.emoji.name) &&
+            user.id !== this.gameEmbed.author.id
+        );
     }
 
     waitForReaction() {
-        this.gameEmbed.awaitReactions((reaction, user) => this.filter(reaction, user), { max: 1, time: 60000, errors: ['time'] })
+        this.gameEmbed
+            .awaitReactions((reaction, user) => this.filter(reaction, user), {
+                max: 1,
+                time: 60000,
+                errors: ["time"],
+            })
             .then(collected => {
                 const reaction = collected.first();
 
                 const snakeHead = this.snake[0];
-                const nextPos = { x: snakeHead.x, y: snakeHead.y };
-                if (reaction.emoji.name === '⬅️') {
+                const nextPos = {x: snakeHead.x, y: snakeHead.y};
+                if (reaction.emoji.name === "⬅️") {
                     let nextX = snakeHead.x - 1;
-                    if (nextX < 0)
-                        nextX = WIDTH - 1;
+                    if (nextX < 0) nextX = WIDTH - 1;
                     nextPos.x = nextX;
-                }
-                else if (reaction.emoji.name === '⬆️') {
+                } else if (reaction.emoji.name === "⬆️") {
                     let nextY = snakeHead.y - 1;
-                    if (nextY < 0)
-                        nextY = HEIGHT - 1;
+                    if (nextY < 0) nextY = HEIGHT - 1;
                     nextPos.y = nextY;
-                }
-                else if (reaction.emoji.name === '⬇️') {
+                } else if (reaction.emoji.name === "⬇️") {
                     let nextY = snakeHead.y + 1;
-                    if (nextY >= HEIGHT)
-                        nextY = 0;
+                    if (nextY >= HEIGHT) nextY = 0;
                     nextPos.y = nextY;
-                }
-                else if (reaction.emoji.name === '➡️') {
+                } else if (reaction.emoji.name === "➡️") {
                     let nextX = snakeHead.x + 1;
-                    if (nextX >= WIDTH)
-                        nextX = 0;
+                    if (nextX >= WIDTH) nextX = 0;
                     nextPos.x = nextX;
                 }
 
-                reaction.users.remove(reaction.users.cache.filter(user => user.id !== this.gameEmbed.author.id).first().id).then(() => {
-                    if (this.isLocInSnake(nextPos)) {
-                        this.gameOver();
-                    }
-                    else {
-                        this.snake.unshift(nextPos);
-                        if (this.snake.length > this.snakeLength)
-                            this.snake.pop();
+                reaction.users
+                    .remove(
+                        reaction.users.cache
+                            .filter(
+                                user => user.id !== this.gameEmbed.author.id
+                            )
+                            .first().id
+                    )
+                    .then(() => {
+                        if (this.isLocInSnake(nextPos)) {
+                            this.gameOver();
+                        } else {
+                            this.snake.unshift(nextPos);
+                            if (this.snake.length > this.snakeLength)
+                                this.snake.pop();
 
-                        this.step();
-                    }
-                });
+                            this.step();
+                        }
+                    });
             })
             .catch(collected => {
                 this.gameOver();
