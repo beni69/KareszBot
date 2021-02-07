383d8a652d70da81330915890c0a055492b982a1
beni69 <73666440+beni69@users.noreply.github.com>
2021-02-07 14:05:55 +0100

    eval now sends results back

--- commands/admin/eval.js
+++ commands/admin/eval.js
@@ -5,9 +5,15 @@ module.exports = {
     run: ({message, args, text, client, prefix, instance}) => {
         const config = require("../../config.json");
         const cmdlog = require("../../features/commandLog.js");
-        if (message.author.id == config.owner.id) {
-            eval(text);
-        }
+        const yargs = require("yargs/yargs");
+        const argv = yargs(text).argv;
+
+        if (message.author.id != config.owner.id) return;
+
+        const cmd = argv._.join(" ");
+
+        if (argv.r || argv.raw) eval(cmd);
+        else message.channel.send(eval(cmd));
 
         cmdlog.Log(client, message);
     },
