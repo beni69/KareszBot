5da5a6dd5190757de69a755432e927ec8d93a04a
beni69 <73666440+beni69@users.noreply.github.com>
2021-03-19 20:01:53 +0100

    👾handler.sync is deprecated

--- src/commands/admin/sync.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from "@beni69/cmd";
-
-export const command = new Command(
-    { names: "sync", adminOnly: true, react: "👌" },
-    async ({ handler }) => {
-        await handler.sync();
-    }
-);
