b4b0b6ab115ca6de84e6975c5ff0a999a8332810
beni69 <73666440+beni69@users.noreply.github.com>
2021-05-24 16:26:56 +0200

    halal

--- src/commands/moderation/kill.ts
+++ src/commands/moderation/kill.ts
@@ -2,6 +2,8 @@ import { Command } from "@beni69/cmd";
 import { GuildMember } from "discord.js";
 import { saveRoles } from ".";
 
+const KILLCHANCE = 6.9;
+
 export const command = new Command(
     {
         names: ["kill", "kick"],
@@ -13,77 +15,90 @@ export const command = new Command(
         blacklist: ["824954982989299722"],
     },
     async ({ message, client, handler, logger }) => {
-        // suicide
-        if (Math.floor(Math.random() * 6.9) == 0) {
-            if (
-                handler.getOpts.admins.has(message.author.id) ||
-                message.author.id == message.guild?.ownerID
-            ) {
-                message.channel.send(
-                    "You would have been killed, if you weren't the server owner. However, the cooldown still applies."
-                );
-                return false;
-            }
-
-            await saveRoles(message.member as GuildMember);
-
-            message.member
-                ?.kick("Tried to kill, but failed.")
-                .catch(err =>
-                    logger
-                        ? logger.log(message, [
-                              "$authorTag$",
-                              " in ",
-                              "$channelTag$",
-                              ":\t",
-                              "self kill failed.",
-                          ])
-                        : null
-                );
-
-            message.author.send(
-                `So unlucky! You shot yourself and have been kicked from **${message.guild?.name}**.\nThere was a 1 in 6.9 chance of that happening. Better luck next time! 🤷‍♂️`
-            );
-            message.channel.send(
-                `${message.author.tag} looked inside the barrel of their own gun to see if it works. They (obviously) died. Such a dumbass.`
-            );
+        if (!message.mentions.users.size) {
+            message.channel.send("Next time mention someone you dumbass");
+            return false;
         }
-        // normal kill
-        else {
-            const target = message.mentions.users.first();
-            if (!target) {
-                message.channel.send("Mention someone you dumbass.");
-                return false;
-            }
-            const targetMember = message.guild?.members.cache.get(
-                target!.id
-            ) as GuildMember;
 
-            if (handler.getOpts.admins.has(target.id)) {
-                message.channel.send("Bro I won't kill my owner.");
-                return false;
-            } else if (message.guild?.ownerID == target.id) {
-                message.channel.send("Sadly, you can't kick the server owner.");
-                return false;
-            } else if (client.user?.id == target.id) {
-                message.channel.send("bruh i wont commit oof");
-                return false;
-            }
+        const sk = selfKill();
 
-            await saveRoles(targetMember);
+        const target = (sk
+            ? message.member
+            : message.mentions.members?.first()) as GuildMember;
+        const mention = message.mentions.members?.first() as GuildMember;
 
-            targetMember?.kick(`Killed by ${message.author.tag}`).catch(err => {
-                message.channel.send("There was an error");
+        // unkillable
+        if (
+            handler.getOpts.admins.has(target.id) ||
+            target.id == message.guild?.ownerID
+        ) {
+            message.channel.send(
+                sk
+                    ? "You would have been killed, if you weren't the server owner. However, the cooldown still applies."
+                    : "It's impossible to kill the server owner. However, the cooldown still applies."
+            );
+            return;
+        }
+
+        if (target.id === client.user?.id) {
+            message.channel.send("I wont kill myself");
+            return false;
+        } else if (!target.manageable) {
+            message.channel.send(
+                "I don't have the permissions to kill that user."
+            );
+            return false;
+        }
+
+        await saveRoles(target);
+
+        await target
+            .kick(
+                sk
+                    ? `Tried to kill ${mention.user.tag} but failed.`
+                    : `Killed by ${message.author.tag}`
+            )
+            .catch(err =>
                 logger
                     ? logger.log(message, [
                           "$authorTag$",
                           " in ",
                           "$channelTag$",
                           ":\t",
-                          "kill failed.",
+                          `${sk ? "self" : ""} kill failed.`,
                       ])
-                    : null;
-            });
+                    : null
+            );
+
+        if (sk) {
+            message.author
+                .send(
+                    `So unlucky! You shot yourself and have been kicked from **${message.guild?.name}**.\nThere was a 1 in ${KILLCHANCE} chance of that happening. Better luck next time! 🤷‍♂️`
+                )
+                .catch(err => {});
+            message.channel.send(
+                `${message.author.tag} looked inside the barrel of their own gun to see if it works. They *(obviously)* died. Such a dumbass.`
+            );
+        } else {
+            target.user
+                .send(
+                    `You have just been brutally murdered by ${message.author.tag}`
+                )
+                .catch(err => {});
         }
+
+        const invite = await message.guild?.channels.cache
+            .first()
+            ?.createInvite({
+                maxUses: 1,
+                unique: false,
+                reason: `Invite for ${target.user.tag} after they were killed by ${message.author.tag}`,
+            });
+
+        target.user
+            .send(`Dont worry tho, you can come back.\n${invite}`)
+            .catch(err => {});
     }
 );
+
+const selfKill = () => Math.floor(Math.random() * KILLCHANCE) == 0;
