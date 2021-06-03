b5d3990fa855b99cf48fe3cf21f03773f8e633a7
beni69 <73666440+beni69@users.noreply.github.com>
2021-06-03 14:34:01 +0200

    sheeeesh

--- src/commands/utility/shorten.ts
+++ src/commands/utility/shorten.ts
@@ -2,10 +2,16 @@ import { Command } from "@beni69/cmd";
 import { MessageEmbed } from "discord.js";
 import fetch from "node-fetch";
 
-const URL = "https://api.karesz.xyz/shortener";
+const URL = "https://krsz.me/api/url/create";
+
 export const command = new Command(
     { names: ["shorten", "shortener", "krszme"] },
     async ({ message, argv }) => {
+        const emb = new MessageEmbed()
+            .setTimestamp()
+            .setTitle("krsz.me")
+            .setURL("https://krsz.me");
+
         const text = argv._.join(" ");
 
         const body = { dest: text };
@@ -19,16 +25,20 @@ export const command = new Command(
         console.log({ data });
 
         if (!res.ok) {
-            message.channel.send("Sorry, something went wrong");
+            let error = "Sorry, something went wrong";
+
+            switch (data.code) {
+                case 10001:
+                    error = "Please provide a valid link.";
+                    break;
+            }
+
+            emb.setColor("RED").setDescription(error);
+            message.channel.send(emb);
             return false;
         }
 
-        const emb = new MessageEmbed()
-            .setTimestamp()
-            .setColor("GREEN")
-            .setTitle("krsz.me")
-            .setURL("https://krsz.me")
-            .setDescription(data.created.url);
+        emb.setColor("GREEN").setDescription(data.url);
         message.channel.send(emb);
     }
 );
